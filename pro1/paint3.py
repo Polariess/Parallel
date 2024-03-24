2876ce6430e0c62584507bf9b3184877c633c055
Polariess <112191663+Polariess@users.noreply.github.com>
2024-03-24 09:22:39 +0800

    Update paint3.py

--- pro1/paint3.py
+++ pro1/paint3.py
@@ -4,17 +4,27 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.family': 'Microsoft YaHei'})  # 这里使用微软雅黑字体，根据需要可以修改成其他字体
 
 # 数据
-labels = ['2^8', '2^9', '2^10', '2^11', '2^12']
-speedup_ratio = [1.21, 1.22, 1.39, 2.14, 2.99]
+labels = ['$2^{16}$', '$2^{17}$', '$2^{18}$', '$2^{19}$', '$2^{20}$']
+trivial_algorithm = [0.1076, 0.2486, 0.4952, 0.9727, 1.9903]
+double_linked = [0.0574, 0.1306, 0.2593, 0.5042, 1.0380]
+recursive_call = [0.0823, 0.1792, 0.3514, 0.7038, 1.4001]
 
 # 绘制折线图
-plt.plot(labels, speedup_ratio, linestyle='--',marker='o')
+plt.plot(labels, trivial_algorithm, marker='o', label='平凡算法')
+plt.plot(labels, double_linked, marker='s', label='双链路式')
+plt.plot(labels, recursive_call, marker='D', label='类递归调用')
 
 # 添加标题和标签
-plt.title('加速比增长曲线')
+plt.title('不同算法与数据规模下的性能对比')
 plt.xlabel('数据规模')
-plt.ylabel('加速比')
+plt.ylabel('平均时间(MS)')
+plt.legend()
+
+# 显示网格
 plt.grid(True)
+
+# 保存为pdf文件
+plt.savefig('add1.pdf')
 
 # 显示图表
 plt.show()
