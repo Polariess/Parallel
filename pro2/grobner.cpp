9f43e8152c52abfb2a077b5326a7fb49e7eaacc4
Polariess <112191663+Polariess@users.noreply.github.com>
2024-04-22 23:21:39 +0800

    Add files via upload

---
+++ pro2/grobner.cpp
@@ -0,0 +1,328 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<map>
+#include<time.h>
+#include<windows.h>
+#include<tmmintrin.h>
+#include<xmmintrin.h>
+#include<emmintrin.h>
+#include<pmmintrin.h>
+#include<smmintrin.h>
+#include<nmmintrin.h>
+#include<immintrin.h>
+using namespace std;
+
+const int T=5;//运行轮数 
+const int maxsize = 3000;
+const int maxrow = 3000;
+const int numBasis = 90000;
+
+map<int, int*>iToBasis;
+map<int, int>iToFirst;
+map<int, int*>ans;
+
+fstream RowFile("被消元行.txt", ios::in | ios::out);
+fstream BasisFile("消元子.txt", ios::in | ios::out);
+
+int gRows[maxrow][maxsize];
+int gBasis[numBasis][maxsize];
+
+void reset() {
+	memset(gRows, 0, sizeof(gRows));
+	memset(gBasis, 0, sizeof(gBasis));
+	RowFile.close();
+	BasisFile.close();
+	RowFile.open("被消元行.txt", ios::in | ios::out);
+	BasisFile.open("消元子.txt", ios::in | ios::out);
+	iToBasis.clear();
+	iToFirst.clear();
+	ans.clear();
+}
+
+void readBasis() {
+	for (int i = 0; i < maxrow; i++) {
+		if (BasisFile.eof()) {
+			return;
+		}
+		string tmp;
+		bool flag = false;
+		int row = 0;
+		getline(BasisFile, tmp);
+		stringstream s(tmp);
+		int pos;
+		while (s >> pos) {
+			//cout << pos << " ";
+			if (!flag) {
+				row = pos;
+				flag = true;
+				iToBasis.insert(pair<int,int*>(row, gBasis[row]));
+			}
+			int index = pos / 32;
+			int offset = pos % 32;
+			gBasis[row][index] = gBasis[row][index]| (1 << offset);
+		}
+		flag = false;
+		row = 0;
+	}
+}
+
+int readRowsFrom(int pos) {
+	iToFirst.clear();
+	if (RowFile.is_open())
+		RowFile.close();
+	RowFile.open("被消元行.txt", ios::in | ios::out);
+	memset(gRows, 0, sizeof(gRows));
+	string line;
+	for (int i = 0; i < pos; i++) {
+		getline(RowFile, line);
+	}
+	for (int i = pos; i < pos + maxsize; i++) {
+		int tmp;
+		getline(RowFile, line);
+		if (line.empty()) {
+			cout << "End of File!" << endl;
+			return i;
+		}
+		bool flag = false;
+		stringstream s(line);
+		while (s >> tmp) {
+			if (!flag) {
+				iToFirst.insert(pair<int, int>(i - pos, tmp));
+			}
+			int index = tmp / 32;
+			int offset = tmp % 32;
+			gRows[i-pos][index] = gRows[i-pos][index] | (1 << offset);
+			flag = true;
+		}
+	}
+	return -1;
+
+}
+
+void update(int row) {
+	bool flag = 0;
+	for (int i = maxsize - 1; i >= 0; i--) {
+		if (gRows[row][i] == 0)
+			continue;
+		else {
+			if (!flag)
+				flag = true;
+			int pos = i * 32;
+			int offset = 0;
+			for (int k = 31; k >= 0; k--) {
+				if (gRows[row][i] & (1 << k))
+				{
+					offset = k;
+					break;
+				}
+			}
+			int newfirst = pos + offset;
+			iToFirst.erase(row);
+			iToFirst.insert(pair<int, int>(row, newfirst));
+			break;
+		}
+	}
+	if (!flag) {
+		iToFirst.erase(row);
+	}
+	return;
+}
+
+void writeResult(ofstream& out) {
+	for (auto it = ans.rbegin(); it != ans.rend(); it++) {
+		int* result = it->second;
+		int max = it->first/32 + 1;
+		for (int i = max; i >= 0; i--) {
+			if (result[i] == 0)
+				continue;
+			int pos = i * 32;
+			//int offset = 0;
+			for (int k = 31; k >= 0; k--) {
+				if (result[i] & (1 << k)) {
+					out << k + pos << " ";
+				}
+			}
+		}
+		out << endl;
+	}
+}
+
+void Solve_Normal() {
+	int begin = 0;
+	int flag;
+	while (true) {
+		flag = readRowsFrom(begin);
+		int num = (flag == -1) ? maxsize : flag;
+		for (int i = 0; i < num; i++) {
+			while (iToFirst.find(i) != iToFirst.end()) {
+				int first = iToFirst.find(i)->second;
+				if (iToBasis.find(first) != iToBasis.end()) {
+					int* basis = iToBasis.find(first)->second;
+					for (int j = 0; j < maxsize; j++) {
+						gRows[i][j] = gRows[i][j] ^ basis[j];
+					}
+					update(i);
+				}
+		 		else {
+					for (int j = 0; j < maxsize; j++) {
+						gBasis[first][j] = gRows[i][j];
+					}
+					iToBasis.insert(pair<int,int*>(first, gBasis[first]));
+					ans.insert(pair<int, int*>(first, gBasis[first]));
+					iToFirst.erase(i);
+				}
+			}
+		}
+		if (flag == -1)
+			begin += maxsize;
+		else
+			break;
+	}
+}
+
+void Solve_AVX() {
+	int begin = 0;
+	int flag;
+	
+	while (true) {
+		flag = readRowsFrom(begin);
+		int num = (flag == -1) ? maxsize : flag;
+		for (int i = 0; i < num; i++) {
+			while (iToFirst.find(i) != iToFirst.end()) {
+				int first = iToFirst.find(i)->second;
+				if (iToBasis.find(first) != iToBasis.end()) {
+					int* basis = iToBasis.find(first)->second;
+					int j = 0;
+					for ( ; j+8 < maxsize; j+=8) {
+						__m256i vij = _mm256_loadu_si256((__m256i*)&gRows[i][j]);
+						__m256i vj = _mm256_loadu_si256((__m256i*)&basis[j]);
+						__m256i vx = _mm256_xor_si256(vij, vj);
+						_mm256_storeu_si256((__m256i*)&gRows[i][j],vx);
+					}	
+					for (; j < maxsize; j++) {
+						gRows[i][j] = gRows[i][j] ^ basis[j];
+					}
+					update(i);
+				}
+				else {
+					int j=0;
+					for ( ; j+8 < maxsize; j+=8) {
+						__m256i vij = _mm256_loadu_si256((__m256i*)&gRows[i][j]);
+						_mm256_storeu_si256((__m256i*)&gBasis[first][j],vij);
+					}
+					for ( ; j < maxsize; j++) {
+						gBasis[first][j] = gRows[i][j];
+					}
+					iToBasis.insert(pair<int, int*>(first, gBasis[first]));
+					ans.insert(pair<int, int*>(first, gBasis[first]));
+					iToFirst.erase(i);
+				}
+			}
+		}
+		if (flag == -1)
+			begin += maxsize;
+		else
+			break;
+	}
+}
+
+void Solve_SSE() {
+	int begin = 0;
+	int flag;
+	
+	while (true) {
+		flag = readRowsFrom(begin);
+		int num = (flag == -1) ? maxsize : flag;
+		for (int i = 0; i < num; i++) {
+			while (iToFirst.find(i) != iToFirst.end()) {
+				int first = iToFirst.find(i)->second;
+				if (iToBasis.find(first) != iToBasis.end()) {
+					int* basis = iToBasis.find(first)->second;
+					int j = 0;
+					for ( ; j+4 < maxsize; j+=4) {
+						__m128i vij = _mm_loadu_si128((__m128i*)&gRows[i][j]);
+						__m128i vj = _mm_loadu_si128((__m128i*) & basis[j]);
+						__m128i vx = _mm_xor_si128(vij, vj);
+						_mm_storeu_si128((__m128i*) & gRows[i][j],vx);
+					}
+					for (; j < maxsize; j++) {
+						gRows[i][j] = gRows[i][j] ^ basis[j];
+					}
+					update(i);
+				}
+				else {
+					int j=0;
+					for ( ; j+4 < maxsize; j+=4) {
+						__m128i vij = _mm_loadu_si128((__m128i*)&gRows[i][j]);
+						_mm_storeu_si128((__m128i*) & gBasis[first][j],vij);
+					}
+					for ( ; j < maxsize; j++) {
+						gBasis[first][j] = gRows[i][j];
+					}
+					iToBasis.insert(pair<int, int*>(first, gBasis[first]));
+					ans.insert(pair<int, int*>(first, gBasis[first]));
+					iToFirst.erase(i);
+				}
+			}
+		}
+		if (flag == -1)
+			begin += maxsize;
+		else
+			break;
+	}
+}
+
+int main() {
+	struct timespec start, end;
+    long long elapsed_time; // 用来存储经过的时间（毫秒）
+	long long time1=0, time2=0, time3=0;
+	
+	
+	for (int i = 0; i < T; i++) {
+        ofstream out("result.txt");
+	    ofstream out1("result_avx.txt");
+	    ofstream out2("result_sse.txt");
+        out<<"__________"<<endl;
+        out1<<"__________"<<endl;
+        out2<<"__________"<<endl;
+		
+		readBasis();
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		Solve_Normal();
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+		cout << "Normal time:" << elapsed_time << "ms" << endl;
+		time1 += elapsed_time;
+		writeResult(out1);
+		reset();
+		
+		
+		readBasis();
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		Solve_SSE();
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+		cout << "SSE time:" << elapsed_time << "ms" << endl;
+		time2 += elapsed_time;
+		writeResult(out1);
+		reset();
+		
+	  	readBasis();
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		Solve_AVX();
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+		cout << "AVX time:" << elapsed_time << "ms" << endl;
+		time3 += elapsed_time;
+		writeResult(out1);
+		reset();
+		
+		out.close();
+		out1.close();
+		out2.close();
+	}
+	cout << "time1(Normal):" << time1/T << " time2(SSE):" << time2/T << " time3(AVX):" << time3/T;
+}
+
---
+++ pro2/normal.cpp
@@ -0,0 +1,342 @@
+#include<iostream>
+#include<chrono>
+#include<ratio>
+#include<ctime>
+#include<stdio.h>
+#include<time.h>
+#include <malloc.h>
+#include<emmintrin.h>
+#include<immintrin.h>
+#include<cstring>
+using namespace std;
+const int N=100; 
+const int MAX=1000;
+float a[N][N];
+void Print(float A[][N])
+{
+	for(int i=0;i<N;i++)
+ 	{
+ 		for(int j=0;j<N;j++)std::cout<<a[i][j]<<" ";
+ 		std::cout<<std::endl;
+	}
+	std::cout<<std::endl;	
+}
+
+void Prepare()
+{
+	srand (static_cast <unsigned> (time(0)));
+	//构造原始上三角矩阵:也即算法处理后期望得到的正确结果 
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<i;j++)
+			a[i][j]=0.0;
+		a[i][i]=1.0;
+ 		for(int j=i+1;j<N;j++)
+ 			a[i][j]=rand()%MAX;
+ 	}
+	
+ 	//后面每一行全部加上前面的行，可看作算法的逆向操作
+	//这样构造出的矩阵一定可以化为标准的上三角形式 
+ 	for(int k=0;k<N;k++)
+ 		for(int i=k+1;i<N;i++)
+ 			for(int j=0;j<N;j++)
+ 				a[i][j]=(int)(a[i][j]+a[k][j])%MAX;
+}
+void Solve_Normal(float A[][N])
+{
+	for(int k=0;k<N;k++)
+		for(int i=k+1;i<N;i++)
+		{
+			float u=a[i][k]/a[k][k];
+			for(int j=k+1;j<N;j++)
+			//正确做法应该是0 - n-1，但已知必然化为上三角矩阵，可在后续直接处理 
+				a[i][j]-=u*a[k][j];
+			a[i][k] = 0.0; //化为上三角矩阵，左下角清0 
+		}
+}
+
+
+void Solve_SSE_ALIGN(float A[][N]) {  //对齐的SSE算法
+    for (int k = 0; k < N; k++) {
+        __m128 t1 = _mm_set1_ps(A[k][k]);
+        int j = k+1;
+
+        //对非对齐部分进行串行处理 
+        while ((long long)(&A[k][j])%16)
+        {
+            A[k][j] = A[k][j] / A[k][k];
+            j++;
+        }
+        //cout << &m[k][j]<<endl;
+        for ( ; j + 4 <= N; j += 4) {
+            __m128 t2 = _mm_load_ps(&A[k][j]);   //已对齐，用load和store指令
+            t2 = _mm_div_ps(t2, t1);
+            _mm_store_ps(&A[k][j], t2);
+        }
+        //对行末进行串行处理 
+        for (; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for (int i = k + 1; i < N; i++) {
+            __m128 vik = _mm_set1_ps(A[i][k]);
+            j = k + 1;
+            //下面同理 
+            while ((long long)(&A[k][j])%16)
+            {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+                j++;
+            }
+            for ( ; j + 4 <= N; j += 4) {
+                __m128 vkj = _mm_load_ps(&A[k][j]);
+                __m128 vij = _mm_loadu_ps(&A[i][j]);
+                __m128 vx = _mm_mul_ps(vik, vkj);
+                vij = _mm_sub_ps(vij, vx);
+                _mm_storeu_ps(&A[i][j], vij);
+            }
+            for (; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void Solve_SSE(float A[][N]) {
+    for (int k = 0; k < N; k++) {
+        __m128 t1 = _mm_set1_ps(A[k][k]);
+        int j = 0;
+        for (j = k + 1; j + 4 <= N; j += 4) {
+            __m128 t2 = _mm_loadu_ps(&A[k][j]);
+            t2 = _mm_div_ps(t2, t1);
+            _mm_storeu_ps(&A[k][j], t2);
+        }
+        for (; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for (int i = k + 1; i < N; i++) {
+            __m128 vik = _mm_set1_ps(A[i][k]);
+            for (j = k + 1; j + 4 <= N; j += 4) {
+                __m128 vkj = _mm_loadu_ps(&A[k][j]);
+                __m128 vij = _mm_loadu_ps(&A[i][j]);
+                __m128 vx = _mm_mul_ps(vik, vkj);
+                vij = _mm_sub_ps(vij, vx);
+                _mm_storeu_ps(&A[i][j], vij);
+            }
+            for (; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void Solve_AVX(float A[][N]) {
+    for (int k = 0; k < N; k++) {
+        __m256 t1 = _mm256_set1_ps(A[k][k]);
+        int j = 0;
+        for (j = k + 1; j + 8 <= N; j += 8) {
+            __m256 t2 = _mm256_loadu_ps(&A[k][j]);
+            t2 = _mm256_div_ps(t2, t1);
+            _mm256_storeu_ps(&A[k][j], t2);
+        }
+        for (; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for (int i = k + 1; i < N; i++) {
+            __m256 vik = _mm256_set1_ps(A[i][k]);
+            for (j = k + 1; j + 8 <= N; j += 8) {
+                __m256 vkj = _mm256_loadu_ps(&A[k][j]);
+                __m256 vij = _mm256_loadu_ps(&A[i][j]);
+                __m256 vx = _mm256_mul_ps(vik, vkj);
+                vij = _mm256_sub_ps(vij, vx);
+                _mm256_storeu_ps(&A[i][j], vij);
+            }
+            for (; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void Solve_AVX_ALIGN(float A[][N]) {
+    for (int k = 0; k < N; k++) {
+        __m256 t1 = _mm256_set1_ps(A[k][k]);
+        int j = k+1;
+        while ((long long)(&A[k][j]) % 32)
+        {
+            A[k][j] = A[k][j] / A[k][k];
+            j++;
+        }
+        for ( ; j + 8 <= N; j += 8) {
+            __m256 t2 = _mm256_load_ps(&A[k][j]);
+            t2 = _mm256_div_ps(t2, t1);
+            _mm256_store_ps(&A[k][j], t2);
+        }
+        for ( ; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for (int i = k + 1; i < N; i++) {
+            __m256 vik = _mm256_set1_ps(A[i][k]);
+            j = k + 1;
+            while ((long long)(&A[k][j]) % 32)
+            {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+                j++;
+            }
+            for ( ; j + 8 <= N; j += 8) {
+                __m256 vkj = _mm256_load_ps(&A[k][j]);
+                __m256 vij = _mm256_loadu_ps(&A[i][j]);
+                __m256 vx = _mm256_mul_ps(vik, vkj);
+                vij = _mm256_sub_ps(vij, vx);
+                _mm256_storeu_ps(&A[i][j], vij);
+            }
+            for ( ; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void Solve_AVX_512(float A[][N]) {
+    for (int k = 0; k < N; k++) {
+        __m512 t1 = _mm512_set1_ps(A[k][k]);
+        int j = 0;
+        for (j = k + 1; j + 16 <= N; j += 16) {
+            __m512 t2 = _mm512_loadu_ps(&A[k][j]);
+            t2 = _mm512_div_ps(t2, t1);
+            _mm512_storeu_ps(&A[k][j], t2);
+        }
+        for (; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for (int i = k + 1; i < N; i++) {
+            __m512 vik = _mm512_set1_ps(A[i][k]);
+            for (j = k + 1; j + 16 <= N; j += 16) {
+                __m512 vkj = _mm512_loadu_ps(&A[k][j]);
+                __m512 vij = _mm512_loadu_ps(&A[i][j]);
+                __m512 vx = _mm512_mul_ps(vik, vkj);
+                vij = _mm512_sub_ps(vij, vx);
+                _mm512_storeu_ps(&A[i][j], vij);
+            }
+            for (; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void Solve_AVX_512_ALIGN(float A[][N]) {
+    for (int k = 0; k < N; k++) {
+        __m512 t1 = _mm512_set1_ps(A[k][k]);
+        int j = k+1;
+        while ((long long)(&A[k][j]) % 64)
+        {
+            A[k][j] = A[k][j] / A[k][k];
+            j++;
+        }
+        for ( ; j + 16 <= N; j += 16) {
+            __m512 t2 = _mm512_load_ps(&A[k][j]);
+            t2 = _mm512_div_ps(t2, t1);
+            _mm512_store_ps(&A[k][j], t2);
+        }
+        for ( ; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for (int i = k + 1; i < N; i++) {
+            __m512 vik = _mm512_set1_ps(A[i][k]);
+            j = k + 1;
+            while ((long long)(&A[k][j]) % 64)
+            {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+                j++;
+            }
+            for ( ; j + 16 <= N; j += 16) {
+                __m512 vkj = _mm512_load_ps(&A[k][j]);
+                __m512 vij = _mm512_loadu_ps(&A[i][j]);
+                __m512 vx = _mm512_mul_ps(vik, vkj);
+                vij = _mm512_sub_ps(vij, vx);
+                _mm512_storeu_ps(&A[i][j], vij);
+            }
+            for ( ; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+int main()
+{
+	struct timespec start, end;
+    long long elapsed_time; // 用来存储经过的时间（毫秒）
+
+    // 打印当前数据规模
+    printf("当前数据规模为:%d\n", N);
+
+    // 平凡算法计时
+    Prepare();
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Solve_Normal(a);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+    printf("平凡算法用时:%lldms\n", elapsed_time);
+
+    // SSE无对齐计时
+    Prepare();
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Solve_SSE(a);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+    printf("SSE(无对齐)用时:%lldms\n", elapsed_time);
+
+    // SSE对齐计时
+    Prepare();
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Solve_SSE_ALIGN(a);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+    printf("SSE(对齐)用时:%lldms\n", elapsed_time);
+    
+    // AVX无对齐计时
+    Prepare();
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Solve_AVX(a);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+    printf("AVX(无对齐)用时:%lldms\n", elapsed_time);
+
+    // AVX对齐计时
+    Prepare();
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Solve_AVX_ALIGN(a);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+    printf("AVX(对齐)用时:%lldms\n", elapsed_time);
+
+//	return 0;
+	
+    // AVX-512无对齐计时
+    Prepare();
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Solve_AVX_512(a);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+    printf("AVX-512(无对齐)用时:%lldms\n", elapsed_time);
+
+    // AVX-512对齐计时
+    Prepare();
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    Solve_AVX_512_ALIGN(a);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    elapsed_time = (end.tv_sec - start.tv_sec) * 1000LL + (end.tv_nsec - start.tv_nsec) / 1000000;
+    printf("AVX-512(对齐)用时:%lldms\n", elapsed_time);
+	return 0;
+}
