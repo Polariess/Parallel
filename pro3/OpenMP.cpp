2080f3ab57f724058478e0bade63b4d4a4a15c6a
Polariess <112191663+Polariess@users.noreply.github.com>
2024-05-02 15:12:11 +0800

    Add files via upload

---
+++ pro3/OpenMP.cpp
@@ -0,0 +1,374 @@
+#include<iostream>
+#include<windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include<tmmintrin.h>
+#include<xmmintrin.h>
+#include<emmintrin.h>
+#include<pmmintrin.h>
+#include<smmintrin.h>
+#include<nmmintrin.h>
+#include<immintrin.h>
+using namespace std;
+#define N 1000
+#define NUM_THREADS 8
+#define T 5
+float A[N][N];
+
+long long head, tail, freq, time_, time_base_;
+
+
+void Prepare() {
+    for (int i = 0; i < N; i++) {
+        A[i][i] = 1.0;
+        for (int j = i + 1; j < N; j++) {
+            A[i][j] = rand() % 1000;
+        }
+
+    }
+    for (int k = 0; k < N; k++) {
+        for (int i = k + 1; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] += A[k][j];
+                A[i][j] = (int)A[i][j] % 1000;
+            }
+        }
+    }
+}
+void print() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void LU() {
+    int i, j, k;
+    for (k = 0; k < N; k++) {
+        for (j = k + 1; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for ( i = k + 1; i < N; i++) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void LU_omp() {
+    int i =0 , j = 0, k = 0;
+    float tmp = 0;
+    #pragma omp parallel  num_threads(NUM_THREADS),private(i,j,k,tmp)
+    for (k = 0; k < N; k++) {
+        #pragma omp single
+        { tmp = A[k][k];
+        for (j = k + 1; j < N; j++) {
+            A[k][j] = A[k][j] / tmp;
+        }
+        A[k][k] = 1.0;
+        }
+
+        #pragma omp for
+        for (i = k + 1; i < N; i++) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void LU_omp_guided() {
+    int i = 0, j = 0, k = 0;
+    float tmp = 0;
+#pragma omp parallel  num_threads(NUM_THREADS),private(i,j,k,tmp)
+    for (k = 0; k < N; k++) {
+#pragma omp single
+        { tmp = A[k][k];
+        for (j = k + 1; j < N; j++) {
+            A[k][j] = A[k][j] / tmp;
+        }
+        }
+        A[k][k] = 1.0;
+#pragma omp for schedule(guided)
+        for (i = k + 1; i < N; i++) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void LU_omp_dynamic() {
+    int i = 0, j = 0, k = 0;
+    float tmp = 0;
+#pragma omp parallel  num_threads(NUM_THREADS),private(i,j,k,tmp)
+    for (k = 0; k < N; k++) {
+#pragma omp single
+        { tmp = A[k][k];
+        for (j = k + 1; j < N; j++) {
+            A[k][j] = A[k][j] / tmp;
+        }
+        }
+        A[k][k] = 1.0;
+#pragma omp for schedule(dynamic)
+        for (i = k + 1; i < N; i++) {
+            for (j = k + 1; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+
+
+void avx_omp_static() {
+    int i = 0, j = 0, k = 0;
+
+#pragma omp parallel  num_threads(NUM_THREADS),private(i,j,k)
+    for (k = 0; k < N; k++) {
+#pragma omp single
+        {
+            __m256 t1 = _mm256_set1_ps(A[k][k]);
+            int j = 0;
+            for (j = k + 1; j + 8 <= N; j += 8) {
+                __m256 t2 = _mm256_loadu_ps(&A[k][j]);
+                t2 = _mm256_div_ps(t2, t1);
+                _mm256_storeu_ps(&A[k][j], t2);
+            }
+            for (; j < N; j++) {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+        }
+#pragma omp for schedule(static)
+        for (i = k + 1; i < N; i++) {
+            __m256 vik = _mm256_set1_ps(A[i][k]);
+            for (j = k + 1; j + 8 <= N; j += 8) {
+                __m256 vkj = _mm256_loadu_ps(&A[k][j]);
+                __m256 vij = _mm256_loadu_ps(&A[i][j]);
+                __m256 vx = _mm256_mul_ps(vik, vkj);
+                vij = _mm256_sub_ps(vij, vx);
+                _mm256_storeu_ps(&A[i][j], vij);
+            }
+            for (; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void avx_omp_dynamic() {
+    int i = 0, j = 0, k = 0;
+
+#pragma omp parallel  num_threads(NUM_THREADS),private(i,j,k)
+    for (k = 0; k < N; k++) {
+#pragma omp single
+        {
+            __m256 t1 = _mm256_set1_ps(A[k][k]);
+            int j = 0;
+            for (j = k + 1; j + 8 <= N; j += 8) {
+                __m256 t2 = _mm256_loadu_ps(&A[k][j]);
+                t2 = _mm256_div_ps(t2, t1);
+                _mm256_storeu_ps(&A[k][j], t2);
+            }
+            for (; j < N; j++) {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+        }
+#pragma omp for schedule(dynamic)
+        for (i = k + 1; i < N; i++) {
+            __m256 vik = _mm256_set1_ps(A[i][k]);
+            for (j = k + 1; j + 8 <= N; j += 8) {
+                __m256 vkj = _mm256_loadu_ps(&A[k][j]);
+                __m256 vij = _mm256_loadu_ps(&A[i][j]);
+                __m256 vx = _mm256_mul_ps(vik, vkj);
+                vij = _mm256_sub_ps(vij, vx);
+                _mm256_storeu_ps(&A[i][j], vij);
+            }
+            for (; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void avx_optimized() {
+    for (int k = 0; k < N; k++) {
+        __m256 t1 = _mm256_set1_ps(A[k][k]);
+        int j = 0;
+        for (j = k + 1; j + 8 <= N; j += 8) {
+            __m256 t2 = _mm256_loadu_ps(&A[k][j]);
+            t2 = _mm256_div_ps(t2, t1);
+            _mm256_storeu_ps(&A[k][j], t2);
+        }
+        for (; j < N; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for (int i = k + 1; i < N; i++) {
+            __m256 vik = _mm256_set1_ps(A[i][k]);
+            for (j = k + 1; j + 8 <= N; j += 8) {
+                __m256 vkj = _mm256_loadu_ps(&A[k][j]);
+                __m256 vij = _mm256_loadu_ps(&A[i][j]);
+                __m256 vx = _mm256_mul_ps(vik, vkj);
+                vij = _mm256_sub_ps(vij, vx);
+                _mm256_storeu_ps(&A[i][j], vij);
+            }
+            for (; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void avx_omp_guided() {
+    int i = 0, j = 0, k = 0;
+
+#pragma omp parallel  num_threads(NUM_THREADS),private(i,j,k)
+    for (k = 0; k < N; k++) {
+#pragma omp single
+        {
+            __m256 t1 = _mm256_set1_ps(A[k][k]);
+            int j = 0;
+            for (j = k + 1; j + 8 <= N; j += 8) {
+                __m256 t2 = _mm256_loadu_ps(&A[k][j]); 
+                t2 = _mm256_div_ps(t2, t1);
+                _mm256_storeu_ps(&A[k][j], t2);
+            }
+            for (; j < N; j++) {
+                A[k][j] = A[k][j] / A[k][k];
+            }
+            A[k][k] = 1.0;
+        }
+#pragma omp for schedule(guided)
+        for (i = k + 1; i < N; i++) {
+            __m256 vik = _mm256_set1_ps(A[i][k]);
+            for (j = k + 1; j + 8 <= N; j += 8) {
+                __m256 vkj = _mm256_loadu_ps(&A[k][j]);
+                __m256 vij = _mm256_loadu_ps(&A[i][j]);
+                __m256 vx = _mm256_mul_ps(vik, vkj);
+                vij = _mm256_sub_ps(vij, vx);
+                _mm256_storeu_ps(&A[i][j], vij);
+            }
+            for (; j < N; j++) {
+                A[i][j] = A[i][j] - A[i][k] * A[k][j];
+            }
+            A[i][k] = 0;
+        }
+    }
+}
+
+void cal(void(*func)()) {
+    Prepare();
+    QueryPerformanceCounter((LARGE_INTEGER*)&head);
+    func();
+    QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+}
+
+
+int main() {
+	printf("数据规模为:%d\n",N);
+    QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+    
+    time_=0;
+	for(int i=1;i<=T;i++)
+	{
+		cal(LU);
+		time_+=(tail - head) * 1000 / freq;
+	}
+    time_/=T;
+    time_base_=time_;
+    cout << "Normal:" << time_ << "ms" << endl;
+	cout<<"加速比为: "<<(double)time_base_/time_<<endl;
+	 
+	time_=0;
+	for(int i=1;i<=T;i++)
+	{
+		cal(avx_optimized);
+		time_+=(tail - head) * 1000 / freq;
+	}
+    time_/=T;
+    time_base_=time_;
+    cout << "avx:" << time_ << "ms" << endl;
+	cout<<"加速比为: "<<(double)time_base_/time_<<endl;
+	
+	time_=0;
+	for(int i=1;i<=T;i++)
+	{
+		cal(LU_omp);
+		time_+=(tail - head) * 1000 / freq;
+	}
+    time_/=T;
+    time_base_=time_;
+    cout << "OpenMp_static:" << time_ << "ms" << endl;
+	cout<<"加速比为: "<<(double)time_base_/time_<<endl;
+	
+	time_=0;
+	for(int i=1;i<=T;i++)
+	{
+		cal(avx_omp_static);
+		time_+=(tail - head) * 1000 / freq;
+	}
+    time_/=T;
+    time_base_=time_;
+    cout << "avx_OpenMP_static:" << time_ << "ms" << endl;
+	cout<<"加速比为: "<<(double)time_base_/time_<<endl;
+	
+	time_=0;
+	for(int i=1;i<=T;i++)
+	{
+		cal(LU_omp_dynamic);
+		time_+=(tail - head) * 1000 / freq;
+	}
+    time_/=T;
+    time_base_=time_;
+    cout << "OpenMp_dynamic:" << time_ << "ms" << endl;
+	cout<<"加速比为: "<<(double)time_base_/time_<<endl;
+    
+    	
+	time_=0;
+	for(int i=1;i<=T;i++)
+	{
+		cal(avx_omp_dynamic);
+		time_+=(tail - head) * 1000 / freq;
+	}
+    time_/=T;
+    time_base_=time_;
+    cout << "avx_OpenMP_dynamic:" << time_ << "ms" << endl;
+	cout<<"加速比为: "<<(double)time_base_/time_<<endl;
+    
+	time_=0;
+	for(int i=1;i<=T;i++)
+	{
+		cal(LU_omp_guided);
+		time_+=(tail - head) * 1000 / freq;
+	}
+    time_/=T;
+    time_base_=time_;
+    cout << "OpenMp_guided:" << time_ << "ms" << endl;
+	cout<<"加速比为: "<<(double)time_base_/time_<<endl;
+    
+	time_=0;
+	for(int i=1;i<=T;i++)
+	{
+		cal(avx_omp_guided);
+		time_+=(tail - head) * 1000 / freq;
+	}
+    time_/=T;
+    time_base_=time_;
+    cout << "avx_OpenMP_guided:" << time_ << "ms" << endl;
+	cout<<"加速比为: "<<(double)time_base_/time_<<endl;
+}
